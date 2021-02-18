8b771ed59944f77b9b347108a378877ade90c446
Milan Stojanovic <milans@UG.KTH.SE>
2021-02-18 21:15:40 +0100

    Add project files.

---
+++ APIInterface.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+namespace II1302
+{
+    class APIInterface
+    {
+        // Speech recognition from microphone.
+        public static async Task RecognitionWithMicrophoneAsync()
+        {
+            var config = SpeechConfig.FromSubscription("eb3c034bbe4d4b5ab7812591856fe1ad", "westeurope");
+
+            // Creates a speech recognizer using microphone as audio input.
+            using (var recognizer = new SpeechRecognizer(config))
+            {
+                // Starts recognizing.
+                Console.WriteLine("Say something...");
+
+                // Starts speech recognition, and returns after a single utterance is recognized. The end of a
+                // single utterance is determined by listening for silence at the end or until a maximum of 15
+                // seconds of audio is processed.  The task returns the recognition text as result.
+                var result = await recognizer.RecognizeOnceAsync().ConfigureAwait(false);
+
+                // Checks result.
+                if (result.Reason == ResultReason.RecognizedSpeech)
+                {
+                    Console.WriteLine($"RECOGNIZED: Text={result.Text}");
+                }
+                else if (result.Reason == ResultReason.NoMatch)
+                {
+                    Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                }
+                else if (result.Reason == ResultReason.Canceled)
+                {
+                    var cancellation = CancellationDetails.FromResult(result);
+                    Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                    if (cancellation.Reason == CancellationReason.Error)
+                    {
+                        Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                        Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                        Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace II1302
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var prompt = "Your choice (0: Stop): ";
+
+            Console.WriteLine("Press 1 to record audio and translate");
+
+            Console.Write(prompt);
+
+            ConsoleKeyInfo x;
+            do
+            {
+                x = Console.ReadKey();
+                Console.WriteLine("");
+                switch (x.Key)
+                {
+                    case ConsoleKey.D1:
+                        APIInterface.RecognitionWithMicrophoneAsync().Wait();
+                        break;
+                }
+            } while (x.Key != ConsoleKey.D0);
+        }
+    }
+}
